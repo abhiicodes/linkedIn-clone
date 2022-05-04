bba07c2d6f9c6ca33f4250b451c3287258ea628f
Abhishek <itzzabhi99@gmail.com>
2022-05-04 11:18:28 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="App">
+     
+      {/* Header */}
+      <Header/>
+
+
+
+      
+      {/* app body */}
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
+    </div>
+  );
+}
+
+export default App;
